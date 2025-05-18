723c4065d6c904b9e2387e037c5164bf0e9cca20
kryuch <oleg.kryuchin@irlix.ru>
2025-05-19 01:18:26 +0300

    Добавление фильтра

--- src/main/java/ru/aa/sov/controller/web/ItemsController.java
+++ src/main/java/ru/aa/sov/controller/web/ItemsController.java
@@ -8,7 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.aa.sov.dto.Magnet;
+import ru.aa.sov.service.FormsService;
+import ru.aa.sov.service.LocalService;
 import ru.aa.sov.service.MagnetService;
+import ru.aa.sov.service.TownsService;
+import ru.aa.sov.service.TypesService;
 import ru.aa.sov.util.PageDataBuilder;
 
 import java.util.Optional;
@@ -19,6 +23,10 @@ import java.util.Optional;
 public class ItemsController {
 
     private final MagnetService magnetService;
+    private final FormsService formsService;
+    private final TypesService typesService;
+    private final LocalService localService;
+    private final TownsService townsService;
 
     @GetMapping("/list")
     public String index(
@@ -30,6 +38,10 @@ public class ItemsController {
     ) {
         Page<Magnet> items = magnetService.getAll(PageDataBuilder.createPage(page, size, sort, dir));
         model = PageDataBuilder.addPaginationToModel(model, items.getTotalPages());
+        model.addAttribute("forms", formsService.items());
+        model.addAttribute("types", typesService.items());
+        model.addAttribute("locals", localService.items());
+        model.addAttribute("regions", townsService.regions());
         model.addAttribute("items", items);
         model.addAttribute("page", "items/list");
         return "page";
